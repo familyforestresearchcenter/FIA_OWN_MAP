3de2b24cef33439124f67873f76d02dd7e06c8d6
Vance Harris <53877381+harrisv25@users.noreply.github.com>
2025-02-25 09:04:02 -0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -8,9 +8,9 @@ from zipfile import ZipFile
 sys.path.insert(0, os.path.abspath('.'))
 
 from utils.helpers import *
-from Name_Matching import *
+from Name_Matching_2_8 import *
 from Classify_Unknowns import *
-from Land_Analysis import *
+# from Land_Analysis import *
 # from Summary_Script import *
 import shutil
 
@@ -21,10 +21,9 @@ if __name__ == '__main__':
     exec(open("Preprocessing.py").read())
     exec(open("Classify_Unknowns.py").read())
     exec(open("Name_Matching_2_8.py").read())
-    os.remove(f'./data/new_parallel_state_temp.csv')
     exec(open("Summary_Script.py").read())
-    os.remove(f'./data/Full_Data_Table.csv')
     exec(open("New_Map_Data.py").read())
+    exec(open("Last_Overlay.py").read())
     state_name = [i for i in os.listdir('./data') if len(i) == 2][0]
     shutil.make_archive(f'./data/{state_name}', 'zip', f'./data/{state_name}')
     shutil.rmtree(f'./data/{state_name}')
